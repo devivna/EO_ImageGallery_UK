502415403969e7a04f381d1fe17db39f9b7dffd6
Nadia <ivashchenko.nadia@gmail.com>
2023-02-24 20:55:33 +0200

    Initial Commit

---
+++ EO_ImageGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EO_ImageGallery
+//
+//  Created by Students on 24.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
