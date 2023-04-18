ddc283be44bf092873c7d132beb97479ac1a13e5
Nadia Ivashchenko <ivashchenko.nadiia@gmail.com>
2023-04-18 15:52:01 +0300

    add images

--- EO_ImageGallery/ViewController.swift
+++ EO_ImageGallery/ViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let images = ["nature1", "nature2", "nature3", "nature4", "nature5", "nature6", "nature7", "nature8", "nature9", "nature10", "nature11", "nature12", "nature13", "nature14", "nature15"]
+    
+    @IBOutlet weak var trashImageView: UIImageView!
+    
+    var nextIndex = 0
+    var currentPicture: UIImageView?
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
